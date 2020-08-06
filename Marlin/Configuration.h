cfff3c20fb49fdedd837a2e75c2e42d15541ef0a
tekknix <root@inspiron.org>
2020-08-06 14:05:22 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -769,7 +769,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 750, 750, 15, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 750, 750, 15, 60 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
