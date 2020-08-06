712f172c8d8c70ba788251fb3d7e973da083f068
tekknix <root@inspiron.org>
2020-08-06 14:43:55 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#define CONFIG_EXAMPLES_DIR "Creality/CR-10 V2"
+//#define CONFIG_EXAMPLES_DIR "Creality/CR-10 V2"
 
 /**
  * Creality CD-10 V2 options
@@ -80,7 +80,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "fran6p, CR10-V2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "MoS-tekknix, CR10-V2)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -141,7 +141,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CR10-V2"
+#define CUSTOM_MACHINE_NAME "CR10-V2-UBL"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
